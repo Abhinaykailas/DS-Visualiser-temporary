edca88874e49d221e3776e25f8d3f47aeab9ac63
anirudh-bukka <anirudh.bukka@gmail.com>
2022-05-30 21:13:20 +0530

    2nd Comm - inserted number saved in console now.

--- src/components/visualiser_page/Visualiser.js
+++ src/components/visualiser_page/Visualiser.js
@@ -1,40 +1,55 @@
 import React from "react"
 import styles from "./Visualiser.module.css";
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 // import { FormField } from 'react-form-input-fields';
 
 function Visualiser() {
-  // state 
-  let [insertedNumberObj, default_val] = useState();
+  // // state 
+  // let [insertedNumberObj, default_val] = useState();
 
-  const insertNumber = () => {
-    // insertedNumberObj = 10
-    // console.log('Number inserted')
-    default_val()
+  const { handleSubmit, register } = useForm();
+
+  const onSubmitted = (obj) => {
+    console.log(obj);
   }
 
 // const Visualiser = () => {
   return (
-    <div className={styles["page__container"]}>
-        <form>
-            <div className={styles["form__container"]}>
-              <div className={styles["form__element"]}>
-                <label htmlFor="insert">Insert a number</label>
-              </div>
-              <br></br>
-              <div className={styles["form__element"]}>
-                <input type="number" name = "" id = "username" className="form-control" />
-              </div>
-              <h2 className="text-info display-1">{insertedNumberObj}</h2>
-              <div className={styles["form__button"]}>
-                {/* <button>insert</button> */}
-                {/* <button class="w3-button w3-round-large">Rounder</button> */}
-                <button type="submit" className="btn btn-success d-block mx-auto" onClick={insertNumber}>Insert</button>
-              </div>
-            </div>
-        </form>
+    <div className={styles["form__container"]}>
+      <form onSubmit={ handleSubmit(onSubmitted) }>
+        <div>
+          <div className={styles["for__element"]}>
+            <label htmlFor="">Insert a number</label>
+          </div>
+          <div className={styles["for__element"]}>
+            <input type="number" {...register("node", {required: true})}/>
+          </div>
+        </div>
+        <div className={styles["form__button"]}>
+          <button type="submit">Insert</button>
+        </div>
+      </form>
+    </div>
+    // <div className={styles["page__container"]}>
+    //     <form>
+    //         <div className={styles["form__container"]}>
+    //           <div className={styles["form__element"]}>
+    //             <label htmlFor="insert">Insert a number</label>
+    //           </div>
+    //           <br></br>
+    //           <div className={styles["form__element"]}>
+    //             <input type="number" name = "" id = "username" className="form-control" />
+    //           </div>
+    //           <div className={styles["form__button"]}>
+    //             {/* <button>insert</button> */}
+    //             {/* <button class="w3-button w3-round-large">Rounder</button> */}
+    //             <button type="submit" onSubmit={ handleSubmit(onSubmitted) }>Insert</button>
+    //           </div>
+    //         </div>
+    //     </form>
 
-      </div>
+    //   </div>
   );
 };
 
